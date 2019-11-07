4d76f17d9222d609a1befc2126319279c5a41b52
Trish Ogen <patriciaogen@gmail.com>
2019-11-07 00:00:00 +0000

    Automatically backed up by Learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,6 @@ class Patient < ApplicationRecord
   has_many :doctors, through: :appointments
 
   def num_appointments
-    self.appointments ? self.appointments.count : nil
+    self.appointments ? self.appointments.count : 0
   end
 end
